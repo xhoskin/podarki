13bf88ac8f58dd2501d48a47262ec1300cb06004
xhoskin <xhoskin@gmail.com>
2015-03-05 01:22:49 +0600

    reviews

---
+++ web/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    // custom controls
+    $('.selectpicker').selectpicker();
+    $('input[type="radio"]').ezMark();
+
+    var makeEqualHeight = function(el){
+        var maxHeight = 0;
+        if ( el.length ) {
+            el.each(function(){
+                elHeight = $(this).height();
+                if ( elHeight > maxHeight ) {
+                    maxHeight = elHeight
+                }
+            });
+            el.height( maxHeight );
+        }
+    }
+
+    makeEqualHeight( $('.homepage-teasers>.teaser-item>.text') );
+});
+
+$(window).load(function(){
+    if ( $('.affix-block').length ) {
+        $mainContent = $('.base-grid>.main');
+        $body = $('html').height()
+        $('.affix-block').affix({
+            offset: {
+                top: function () {
+                    return (this.top = $mainContent.offset().top - 20 );
+                },
+                bottom: function () {
+                    return (this.bottom = $body - ($mainContent.offset().top + $mainContent.height()) );
+                }
+            }
+        });
+    }
+});
